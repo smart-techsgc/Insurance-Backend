240efc7d58699666d378400c31774b5881d6fb8f
Emmanuel Eshun <emmanuel.eshun@amalitech.com>
2024-07-07 22:56:17 +0000

    "Added cors dependency and middleware to app.ts, updated package.json and yarn.lock accordingly."

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ import * as dotenv from "dotenv";
 import { landingRouter } from "./routes/landingPage.route";
 import { usersRouter } from "./routes/users.route";
 import { accessRouter } from "./routes/accessLevel.route";
+import cors from "cors";
+
 dotenv.config();
 const app = express();
 const port = process.env.port || 8000;
 app.use(express.json());
 
+app.use(cors());
 app.use("/", landingRouter);
 app.use("/user", usersRouter);
 app.use("/access-level", accessRouter);
